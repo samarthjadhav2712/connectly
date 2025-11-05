7079e223af100f685b4769e730996179a76634b6
Samarth Jadhav <samarth10jadhav@gmail.com>
2025-11-06 03:17:32 +0530

    minor deployment changes commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -6,6 +6,7 @@ import chatRoutes from './routes/chatRoute.js'
 import { connectDB } from './lib/db.js';
 import cookieParser from "cookie-parser"
 import cors from "cors";
+import path from 'path';
 
 dotenv.config(); /// to read the file content . 
 const app = express();
@@ -16,12 +17,22 @@ app.use(cors({
     credentials : true // allow frontend to send cookies 
 }));
 
+const _dirname = path.resolve();
+
 app.use(express.json()); // to parse json data in req body .
 app.use(cookieParser()); //to use jwt token in router .
 
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);
 app.use('/api/chat', chatRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(_dirname , "../frontend/dist")));
+    
+    app.get("*" , (req,res)=>{
+        res.sendFile(path.join(_dirname , "../frontend" , "dist" , "index.html"));
+    });
+}
 
 app.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`);
